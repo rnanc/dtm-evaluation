220b4ea9cfe1e6941bfaaf37e2df5893cb848f83
José Nonato <jose20070007@aluno.cesupa.br>
2020-09-09 09:49:36 -0300

    Tela de medição
    Add tela de medição

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/DTM'
 db = SQLAlchemy(app)
 
+@app.route("/")
+def inicio():
+    return render_template("public/primeiratela.html")
+
+@app.route("/segunda")
+def segunda():
+    return render_template("public/segundatela.html")
+
+@app.route("/final")
+def final():
+    return render_template("public/final.html")
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
