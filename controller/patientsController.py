779d58867feac32b37a3ae526f5ea40e54a5df1c
Renan <renanfigcarneiro@gmail.com>
2020-09-26 18:31:12 -0300

    feat: sign up validations

--- controller/patientsController.py
+++ controller/patientsController.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, url_for
+from flask_jwt_extended import jwt_required
 
 patients_blueprint = Blueprint('patients', __name__, template_folder='templates', static_folder='static')
 
 @patients_blueprint.route('/patients')
+@jwt_required
 def patientsPage():
   return render_template('patientList.html')
