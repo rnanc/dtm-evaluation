cec28cdec16cf4c118d3cc20dda9a897a27c78da
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2020-10-19 20:56:50 -0300

    fix: app, serializer e model created

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from controller.ExamController import exam_blueprint
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jose131997@localhost/DTM'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:jose131997@localhost/DTM'
 app.config['SECRET_KEY'] = 'e7c0596d00d6d1d17e64d6547cd732cf'
 app.config['JWT_SECRET_KEY'] = "1cca2a86e499bc8f16a75000cea3fbc5"
 app.config['JWT_TOKEN_LOCATION'] = "cookies"
--- model/User.py
+++ model/User.py
@@ -24,3 +24,20 @@ class User(db.Model):
     self.registered_number = registered_number
     self.email = email
     self.password = password
+
+  class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    birth_date = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    pain_choice = db.Column(db.Boolean)
+
+    def __init__(self, name, email, phone, birth_date, gender, pain_choice):
+      self.name = name
+      self.email = email
+      self.phone = phone
+      self.birth_date = birth_date
+      self.gender = gender
+      self.pain_choice = pain_choice
