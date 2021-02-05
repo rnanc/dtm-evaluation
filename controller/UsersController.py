15c35df5aa5662e2f23f6f3397f27844013f0e55
FreitasGa <freitasga.01@gmail.com>
2021-02-05 17:15:17 -0300

    fix: Bug where it was possible to register patients with birth date greater than the current; Bug where it was possible to register patients without birth date; Bug in the gender button on patient register; Passwords should have have at least 6 digits; Bug in the console on the register page; Bug on profile page where shows "telefone" instead of "email"; Bug on profile page where was possible to register an email out of cesupa domain; Difficult to read field on dashboard.

--- controller/UsersController.py
+++ controller/UsersController.py
@@ -16,7 +16,9 @@ def profile():
 @jwt_required
 def edit_profile():
   if request.method == "GET":
-    return render_template("edit_profile.html")
+    id = request.cookies.get("user_id")
+    user = Users.query.get(id)
+    return render_template('edit_profile.html', user=user)
   else:
     id = request.cookies.get("user_id")
     user = Users.query.get(id)
@@ -24,21 +26,25 @@ def edit_profile():
     us = UserSchema()
     user_form = request.form.to_dict()
     if user_form != None:
-      if user_form["email"] == user_form["email_confirm"] and user_form["password"] == user_form["password_confirm"]:
-        del user_form["email_confirm"]
-        del user_form["password_confirm"]
-        user_sch = us.load(user_form)
-        user_sch.gen_hash()
-        if user != None:
-          verify = user.verify_password(user_form['password'])
-        if user != None and verify:
-          user_form["password"] = user_sch.password
-          user_query.update(user_form)
-          current_app.db.session.commit()
-          return redirect(url_for('users.profile'))
+      if "@aluno.cesupa.br" in user_form["email"] and "@prof.cesupa.br" in user_form["email"]:
+        if user_form["email"] == user_form["email_confirm"] and user_form["password"] == user_form["password_confirm"]:
+          del user_form["email_confirm"]
+          del user_form["password_confirm"]
+          user_sch = us.load(user_form)
+          user_sch.gen_hash()
+          if user != None:
+            verify = user.verify_password(user_form['password'])
+          if user != None and verify:
+            user_form["password"] = user_sch.password
+            user_query.update(user_form)
+            current_app.db.session.commit()
+            return redirect(url_for('users.profile'))
+          else:
+            flash("Senha errada!", "danger")
+            return redirect(url_for('users.edit_profile'))
         else:
-          flash("Senha errada!", "danger")
+          flash("Campos de confirmação errados!", "danger")
           return redirect(url_for('users.edit_profile'))
       else:
-        flash("Campos de confirmação errados!", "danger")
+        flash("Email precisa ser do domínio CESUPA! Tente novamente cadastrando seu email do CESUPA. ", "danger")
         return redirect(url_for('users.edit_profile'))
