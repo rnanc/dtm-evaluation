eaf2736377851c7b263e15eda10cb596466be640
Elielson68 <elielsonbr.com@gmail.com>
2021-05-21 18:49:20 -0300

    fix: socketio config heroku and local

--- dtm/extensions/middleware.py
+++ dtm/extensions/middleware.py
@@ -4,15 +4,15 @@ from dtm.listeners.exam import open_measurement, shut_measurement
 import os
 
 Payload.max_decode_packets = 1000
-web = {'async_mode': 'eventlet', 'pingTimeout': 900, 'logger': True, 'engineio_logger': True, 'cors_allowed_origins': "*"}
-local = {'pingTimeout': 900, 'cors_allowed_origins': "*"}
-
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = None
+if os.environ.get('FLASK_ENV') == 'development':
+  socketio = SocketIO(cors_allowed_origins="*")
+else:
+  socketio = SocketIO(async_mode='eventlet', pingTimeout=900, logger=True, engineio_logger=True, cors_allowed_origins='*')
 socketio.on_event('open_measurement', open_measurement, namespace='/exam')
 socketio.on_event('shut_measurement', shut_measurement, namespace='/exam')
 
 def init_app(app):
-  #socketio.server_options.update(local if os.environ.get('FLASK_ENV') == 'development' else web)
   socketio.init_app(app)
 
 def start_app(app):
